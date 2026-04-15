fc5486d6a267da62397700bc862d8ed867b4474d
Mohamad <97125106@st.deltion.nl>
2026-04-15 09:45:17 +0200

    Styled the settings screen

--- app/(tabs)/settings/index.tsx
+++ app/(tabs)/settings/index.tsx
@@ -1,28 +1,64 @@
 import React from "react";
-import { Text, Pressable } from "react-native";
+import { Text, Pressable, View, Image, StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useRouter } from "expo-router";
+import { height } from "@expo/ui/jetpack-compose/modifiers";
 
 export default function settings() { 
     const router = useRouter();
 
     return (
         <SafeAreaView>
-            <Pressable
-                onPress={() => {router.push('settings/permissions')}}
-            >
-                <Text>Permissions</Text>
-            </Pressable>
-            <Pressable
-                onPress={() => {router.push('settings/personalization')}}
-            >
-                <Text>Personalization</Text>
-            </Pressable>
-            <Pressable
-                onPress={() => {router.push('settings/about')}}
-            >
-                <Text>About</Text>
-            </Pressable>
+            <Text style={styles.title}>Settings</Text>
+            <View style={{ marginTop: 17 }}>
+                <Pressable
+                    onPress={() => {router.push('settings/permissions')}}
+                    style={styles.pressable}
+                >
+                    <Image source={require('../../../assets/database.png')} style={styles.image}/>
+                    <Text style={styles.pressableText}>Permissions</Text>
+                </Pressable>
+
+                <Pressable
+                    onPress={() => {router.push('settings/personalization')}}
+                    style={styles.pressable}
+                >
+                    <Image source={require('../../../assets/personalization.png')} style={styles.image}/>
+                    <Text style={styles.pressableText}>Personalization</Text>
+                </Pressable>
+
+                <Pressable
+                    onPress={() => {router.push('settings/about')}}
+                    style={styles.pressable}
+                >
+                    <Image source={require('../../../assets/info.png')} style={{ height: 23, width: 23 }}/>
+                    <Text style={styles.pressableText}>About</Text>
+                </Pressable>
+            </View>
         </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    title: {
+        fontSize: 25,
+        fontWeight: 'bold',
+        marginLeft: 13,
+        marginTop: 17
+    },
+    pressable: {
+        flexDirection: 'row',
+        marginTop: 10,
+        marginLeft: 16,
+        gap: 12
+    },
+    image: {
+        height: 26,
+        width: 26
+    },
+    pressableText: {
+        fontSize: 16,
+        color: '#333',
+        fontWeight: '500'
+    }
+})
